b3f626c13c5d00c2920e387aced2f055e71860e1
kawhicurry <1145315261@qq.com>
2022-10-25 13:58:23 +0800

    feat: enable fcitx5 auto switch

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -47,10 +47,10 @@ function autocmd.load_autocmds()
 				[[if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]],
 			},
 			-- Auto toggle fcitx5
-			-- {"InsertLeave", "* :silent", "!fcitx5-remote -c"},
-			-- {"BufCreate", "*", ":silent !fcitx5-remote -c"},
-			-- {"BufEnter", "*", ":silent !fcitx5-remote -c "},
-			-- {"BufLeave", "*", ":silent !fcitx5-remote -c "}
+			{ "InsertLeave", "* :silent", "!fcitx5-remote -c" },
+			{ "BufCreate", "*", ":silent !fcitx5-remote -c" },
+			{ "BufEnter", "*", ":silent !fcitx5-remote -c " },
+			{ "BufLeave", "*", ":silent !fcitx5-remote -c " },
 		},
 		wins = {
 			-- Highlight current line only on focused window
